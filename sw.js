e05a4602ae5e49207e9db1825123d97389592e55
mimimi0mimimi <tempmys@icloud.com>
2026-06-03 00:02:14 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'kyuryo-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
